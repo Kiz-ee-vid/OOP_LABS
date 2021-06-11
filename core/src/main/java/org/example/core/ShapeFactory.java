f23caae05d63cc09223cdc4f1bf9df2d316e2570
Kleptoman <ilyakizeev@gmail.com>
2021-06-11 16:29:43 +0300

    deleting

--- core/src/main/java/org/example/core/ShapeFactory.java
+++
@@ -1,8 +0,0 @@
-package org.example.core;
-
-import java.util.List;
-
-public interface ShapeFactory {
-    Shapes createShape(List<Double> point, SeriaColor outlineColor, SeriaColor brushColor, double lineWeight) ;
-}
-
